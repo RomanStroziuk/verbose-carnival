d2a4b8829ccc738349e35b807d47a4545dd00266
parsl3y <burchakpetro@gmail.com>
2024-11-30 01:45:04 +0200

    Coin MoveFadeDestroyer implement

--- Assets/Code/Runtime/Gameplay/Logic/Coin.cs
+++ Assets/Code/Runtime/Gameplay/Logic/Coin.cs
@@ -1,15 +1,27 @@
 using System.ComponentModel;
 using Code.Runtime.Gameplay.Service.Wallet;
+using Code.Runtime.Gameplay.View;
 using Code.Runtime.Infrastructure.Services.SaveLoad;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Zenject;
 
 namespace Code.Runtime.Gameplay.Logic
 {
     public class Coin : MonoBehaviour, ICollecteble
     {
+        [FormerlySerializedAs("moveFader")] [FormerlySerializedAs("_moveFadeDestroyer")] [SerializeField]
+        private MoveFaderDestroy moveFaderDestroy;
+        
+        [SerializeField]
+        private Rigidbody2D _rigidbody2D;
+        
+        [SerializeField]
+        private Collider2D _collider2D;
+        
         private IWalletService _walletService;
         private ISaveLoadService _saveLoadService;
+        
         public bool IsCollected { get; private set; }
 
         [Inject]
@@ -23,7 +35,11 @@ namespace Code.Runtime.Gameplay.Logic
         {
             _walletService.AddCoin();
             _saveLoadService.SavePrograss();
-            Destroy(gameObject);
+            IsCollected = true;
+            
+            Destroy(_rigidbody2D);
+            _collider2D.enabled = false;
+            moveFaderDestroy.Destroy();
         }
         
         
---
+++ Assets/Code/Runtime/Gameplay/View/MoveFaderDestroy.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
+
+namespace Code.Runtime.Gameplay.View
+{
+    public class MoveFaderDestroy : MonoBehaviour
+    {
+        [SerializeField]
+        private SpriteRenderer _spriteRenderer;
+
+        [SerializeField] 
+        private float _duration = 1;
+
+        [SerializeField] 
+        private float _moveDeltaY = -1;
+        
+        public void Destroy() =>
+        
+                DOTween.Sequence()
+                .Join(Fade())
+                .Join(Move())
+                .OnComplete(() => Destroy(gameObject))
+                .Play();
+
+        
+
+        private Tween Move() =>
+          gameObject
+              .transform.
+              DOMoveY(transform.position.y - _moveDeltaY, _duration)
+              .SetEase(Ease.InOutCubic)
+              .OnComplete(() => Destroy(gameObject));
+              
+        
+        private TweenerCore<Color, Color, ColorOptions> Fade() =>
+        
+            _spriteRenderer
+                .DOFade(0, _duration)
+                .SetEase(Ease.InOutCubic)
+                .SetLink(gameObject)
+                .OnComplete(() => Destroy(gameObject));
+        
+    }
+}
